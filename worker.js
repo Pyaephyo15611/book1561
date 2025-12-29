6999d754afa85cfef0dfc17fa45cfb2e51d3c6bc
Pyae Phyo <pyaephyo15611@gmail.com>
2025-12-29 19:32:42 +0630

    alls

--- worker.js
+++ worker.js
@@ -6,21 +6,7 @@ async function handleRequest(request) {
   const url = new URL(request.url)
   const pathname = url.pathname
   
-  // Handle static assets
-  const staticAssetExtensions = ['.js', '.css', '.png', '.jpg', '.jpeg', '.gif', '.svg', '.ico', '.woff', '.woff2', '.ttf', '.eot']
-  const isStaticAsset = staticAssetExtensions.some(ext => pathname.endsWith(ext))
-  
-  if (isStaticAsset) {
-    // Try to fetch from assets
-    const assetUrl = new URL(pathname, url.origin)
-    return fetch(new Request(assetUrl, request))
-  }
-  
-  if (pathname === '/') {
-    // Serve index.html for root
-    return fetch(new Request(new URL('/index.html', url.origin), request))
-  }
-  
-  // For all other routes, serve index.html to let React Router handle it
+  // For all routes, serve index.html to let React Router handle it
+  // This is the simplest approach that works with Cloudflare Workers assets
   return fetch(new Request(new URL('/index.html', url.origin), request))
 }
