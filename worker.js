64381d31d6ffc01d9ba14dc98cdb004f69f6a243
Pyae Phyo <pyaephyo15611@gmail.com>
2025-12-29 12:55:46 +0630

    Fisxsdss

--- worker.js
+++ worker.js
@@ -4,24 +4,23 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request) {
   const url = new URL(request.url)
+  const pathname = url.pathname
   
   // Handle static assets
   const staticAssetExtensions = ['.js', '.css', '.png', '.jpg', '.jpeg', '.gif', '.svg', '.ico', '.woff', '.woff2', '.ttf', '.eot']
-  const isStaticAsset = staticAssetExtensions.some(ext => url.pathname.endsWith(ext))
+  const isStaticAsset = staticAssetExtensions.some(ext => pathname.endsWith(ext))
   
-  if (isStaticAsset || url.pathname === '/') {
-    // Serve static assets and root normally
-    return fetch(request)
+  if (isStaticAsset) {
+    // Try to fetch from assets
+    const assetUrl = new URL(pathname, url.origin)
+    return fetch(new Request(assetUrl, request))
+  }
+  
+  if (pathname === '/') {
+    // Serve index.html for root
+    return fetch(new Request(new URL('/index.html', url.origin), request))
   }
   
   // For all other routes, serve index.html to let React Router handle it
-  const indexUrl = new URL('/index.html', url.origin)
-  const indexRequest = new Request(indexUrl, {
-    headers: request.headers,
-    method: request.method,
-    body: request.body,
-    redirect: 'follow'
-  })
-  
-  return fetch(indexRequest)
+  return fetch(new Request(new URL('/index.html', url.origin), request))
 }
